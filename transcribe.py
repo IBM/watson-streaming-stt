2ca5d34ad126f74b3e72831230ffbab7cd006e6b
Sean Dague <sean@dague.net>
2016-12-20 09:40:54 -0500

    Initial commit for working streaming audio

---
+++ transcribe.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 IBM
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import base64
+import ConfigParser
+import json
+import thread
+import time
+
+import pyaudio
+import websocket
+from websocket._abnf import ABNF
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+# Even if your default input is multi channel (like a webcam mic),
+# it's really important to only record 1 channel, as the STT service
+# does not do anything useful with stereo. You get a lot of "hmmm"
+# back.
+CHANNELS = 1
+# Rate is important, nothing works without it. This is a pretty
+# standard default. If you have an audio device that requires
+# something different, change this.
+RATE = 44100
+RECORD_SECONDS = 5
+
+
+def read_audio(ws):
+    """Read audio and sent it to the websocket port.
+
+    This uses pyaudio to read from a device in chunks and send these
+    over the websocket wire.
+
+    """
+    p = pyaudio.PyAudio()
+    # NOTE(sdague): if you don't seem to be getting anything off of
+    # this you might need to specify:
+    #
+    #    input_device_index=N,
+    #
+    # Where N is an int. You'll need to do a dump of your input
+    # devices to figure out which one you want.
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        print "Sending packet... %d" % i
+        # NOTE(sdague): we're sending raw binary in the stream, we
+        # need to indicate that otherwise the stream service
+        # interprets this as text control messages.
+        ws.send(data, ABNF.OPCODE_BINARY)
+
+    # Disconnect the audio stream
+    stream.stop_stream()
+    stream.close()
+    print("* done recording")
+
+    # In order to get a final response from STT we send a stop, this
+    # will force a final=True return message.
+    data = {"action": "stop"}
+    ws.send(json.dumps(data).encode('utf8'))
+    # ... which we need to wait for before we shutdown the websocket
+    time.sleep(1)
+    ws.close()
+    # ... and kill the audio device
+    p.terminate()
+
+
+def on_message(self, msg):
+    """Print whatever messages come in."""
+    print msg
+
+
+def on_error(self, error):
+    """Print any errors."""
+    print error
+
+
+def on_close(ws):
+    print "### closed ###"
+
+
+def on_open(ws):
+    """Triggered as soon a we have an active connection."""
+
+    data = {
+        "action": "start",
+        # this means we get to send it straight raw sampling
+        "content-type": "audio/l16;rate=%d" % RATE,
+        "continuous": True,
+        "interim_results": True,
+        "inactivity_timeout": 600,
+        "word_confidence": True,
+        "timestamps": True,
+        "max_alternatives": 3
+    }
+
+    # Send the initial control message which sets expectations for the
+    # binary stream that follows:
+    ws.send(json.dumps(data).encode('utf8'))
+    # Spin off a dedicated thread where we are going to read and
+    # stream out audio.
+    thread.start_new_thread(read_audio, (ws,))
+
+
+def get_auth():
+    config = ConfigParser.RawConfigParser()
+    config.read('speech.cfg')
+    user = config.get('auth', 'username')
+    password = config.get('auth', 'password')
+    return (user, password)
+
+
+def main():
+    # Connect to websocket interfaces
+    headers = {}
+    userpass = ":".join(get_auth())
+    headers["Authorization"] = "Basic " + base64.b64encode(userpass)
+    url = ("wss://stream.watsonplatform.net//speech-to-text/api/v1/recognize"
+           "?model=en-US_BroadbandModel")
+
+    # If you really want to see everything going across the wire,
+    # uncomment this. However realize the trace is going to also do
+    # things like dump the binary sound packets in text in the
+    # console.
+    #
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(url,
+                                header=headers,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    # This gives control over the WebSocketApp. This is a blocking
+    # call, so it won't return until the ws.close() gets called (after
+    # 6 seconds in the dedicated thread).
+    ws.run_forever()
+
+
+if __name__ == "__main__":
+    main()
